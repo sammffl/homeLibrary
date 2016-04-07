16b265be81c5c1ab7fc696a4cc51c554873aa145
SamMFFL <sammffl@icloud.com>
2016-04-07 15:52:53 +0800

    add index

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,20 @@
 var express = require("express");
 var router = express.Router();
-
-router.get("/", function (req, res) {
+router.all("/", function (req, res, next) {
+    console.log("start");
+    next();
+});
+router.get("/", function (req, res, next) {
     res.render("index");
+    next();
+}).post("/", function (req, res, next) {
+    console.log("post");
+    res.send("ok");
+    next();
+});
+
+router.all("/", function (req, res) {
+    console.log("end")
 });
 
 module.exports = router;
