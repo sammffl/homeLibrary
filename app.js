aee114793aa1dd7efa742eb069bf91e6b29f291c
SamMFFL <sammffl@icloud.com>
2016-04-07 13:58:33 +0800

    edit

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
+var admin = express();
+var config = require("./config.json");
 
 app.get('/', function (req, res) {
     res.send('Hello World')
-})
+});
+
+admin.get("/", function (req, res) {
+    res.send('admin page');
+});
+
+app.use("/admin", admin);
+
 
 app.listen(3000);
+console.log('Express started on port 3000');
