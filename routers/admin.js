87a55a8175c3364164515f2ce567b58dd265a581
SamMFFL <sammffl@icloud.com>
2016-04-08 15:03:39 +0800

    使用controller层后不用了

--- routers/admin.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-router.get("/", function (req, res) {
-    //console.log(router.mountpath);
-    res.send('admin page');
-});
-
-module.exports = router;
