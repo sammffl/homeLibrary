4a4a9f3ce99e0801ef5583e162f93addb06f8055
sam.shen <sammffl@icloud.com>
2016-04-10 20:30:24 +0800

    m

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,19 @@
 /**
  * Created by SamMFFL on 2016/4/9.
  */
+
+$(function () {
+    $("#search-menu >li >a").click(function () {
+        var originText = $("#search-btn .text").html();
+        var originType = $("#search-btn").data("type");
+        var currentText = $(this).html();
+        var currentType = $(this).data("type");
+        $("#search-btn .text").html(currentText);
+        $("#search-btn ").data("type", currentType);
+        $(this).html(originText);
+        $(this).data("type", originType);
+    });
+    $("#search-text").keyup(function () {
+        console.log($("#search-btn").data("type"))
+    })
+});
