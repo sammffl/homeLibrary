9704bf4de580d942ddacdde480d626220d508d3d
SamMFFL <sammffl@icloud.com>
2016-04-11 15:55:35 +0800

    添加ip追踪

--- controllers/frontend/homeController.js
+++ controllers/frontend/homeController.js
@@ -1,3 +1,5 @@
+var utils = require("../../common/utils");
 exports.index = function (req, res, next) {
-    res.render("home", {title: "home"});
+    var ip = utils.getClientIp(req).split(":").pop();
+    res.render("home", {title: "home", ip: ip});
 };
