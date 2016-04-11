09ea5f7f2019fe7834f4501031c6179e6d732009
SamMFFL <sammffl@icloud.com>
2016-04-11 18:16:26 +0800

    查找书

--- api/v1/index.js
+++ api/v1/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bookApi = require("./book");
 
 var router = express.Router();
-var config = require("../../config");
+var config = require("../../config")();
 
 //book Tags标签
 router.get('/books/tags', bookApi.tags);
