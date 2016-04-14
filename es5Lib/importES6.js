50f2b8463e50a30bc5fdc6794d0a12e61e75f40a
SamMFFL <sammffl@icloud.com>
2016-04-14 18:15:34 +0800

    ..

---
+++ es5Lib/importES6.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var _utils = require('../common/utils.js');
+
+var _utils2 = _interopRequireDefault(_utils);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var utils = new _utils2.default(); /**
+                                    * Created by Administrator on 2016/4/14.
+                                    */
