4a19397aa53d6f9bcec6dcfce4ecc14cb4af68d2
sam.shen <sammffl@icloud.com>
2016-04-15 22:01:20 +0800

    ++

---
+++ api/v1/film.js
@@ -0,0 +1,8 @@
+/**
+ * Created by SamMFFL on 2016/4/15.
+ */
+var config = require("../../config")();
+
+exports.filmList = function (req, res, next) {
+    res.send('film');
+}
--- api/v1/index.js
+++ api/v1/index.js
@@ -1,18 +1,23 @@
 var express = require("express");
 var bookApi = require("./book");
+var filmApi = require("./film");
 
 var router = express.Router();
 var config = require("../../config")();
 
 //所有接口介绍
-router.get("/",function(req,res,next){
+router.get("/", function (req, res, next) {
     var host = req.rawHeaders[1];
     res.send({
-        "books_tags_from_douban":host+"/api/v1/books/tags",
-        "books_name_list_from_douban":host+"/api/v1/books/:bookname",
+        "books_tags_from_douban": host + "/api/v1/books/tags",
+        "books_name_list_from_douban": host + "/api/v1/books/:bookname",
     })
 });
 //book Tags标签
 router.get('/books/tags', bookApi.tags);
 router.get('/books/:bookname', bookApi.bookList);
 module.exports = router;
+
+
+//film
+router.get('/films/tags',filmApi.filmList);
