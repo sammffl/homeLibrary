98c0ffb1615a87546c2f4ef9c5e4ac711ab05167
SamMFFL <sammffl@icloud.com>
2016-04-19 17:36:00 +0800

    json

--- public/js/ajaxDemo.js
+++ public/js/ajaxDemo.js
@@ -1,3 +1,17 @@
-/**
- * Created by Administrator on 2016/4/19.
- */
+$(function () {
+    $("#btn").click(function () {
+        $.ajax({
+            url: "http://localhost:8801/api/v1/getText",
+            method: "GET",
+            dataType: "json",
+            context: $("#btn"),
+            success: function (data) {
+                if (data) {
+                    $("#txt").val(data.txt);
+                }
+            }
+        }).done(function () {
+            $(this).html("ajax called")
+        });
+    })
+});
